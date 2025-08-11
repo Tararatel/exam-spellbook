000e09a9ae80217cd755bf8d046a68d1d515b1cf
Tararatel <caratel18@mail.ru>
2025-08-11 17:14:23 +0300

    del types

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,13 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import wordsReducer from '@/entities/Words/model/wordsSlice';
-import spellsReducer from '@/entities/Spells/model/spellsSlice';
-import spellComponentsReducer from '@/entities/SpellComponents/model/spellComponentsSlice';
 
 export const store = configureStore({
   reducer: {
-    words: wordsReducer,
-    spells: spellsReducer,
-    spellComponents: spellComponentsReducer,
 
   },
 });
